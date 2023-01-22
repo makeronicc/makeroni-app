1ff92b6c110d274e95b1a1062e48b7d59efa84a9
Eric Muir <eric.muir@xdesign.com>
2023-01-22 13:13:22 +0000

    Add card component

---
+++ resources/views/components/card.blade.php
@@ -0,0 +1,11 @@
+<div class="p-6 sm:px-20 bg-white border-b border-gray-200">
+    <div>
+        <x-jet-application-logo class="block h-12 w-auto" />
+    </div>
+
+    <div class="mt-8 text-2xl">
+        {{ $title }}
+    </div>
+
+    
+</div>
